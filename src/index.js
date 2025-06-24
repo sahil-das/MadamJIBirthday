13dd5b8938072bfa6cc73cf24f05829269c50c92
sahil-das <66070916+sahil-das@users.noreply.github.com>
2025-06-24 23:13:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css'; // global styles or reset
+//import './index.css'; // global styles or reset 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
