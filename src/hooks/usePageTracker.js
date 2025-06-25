4b5aaf2170c18fc90b37795683e53f1a07968e17
sahil <sahildas452@gmail.com>
2025-06-25 22:39:56 +0530

    Update usePageTracker.js

--- src/hooks/usePageTracker.js
+++ src/hooks/usePageTracker.js
@@ -2,8 +2,6 @@
 import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const api_url = 'https://visitor-traker.onrender.com/api/activity/log';
-//const api_url = 'http://localhost:5000/api/activity/log'; // Change to your actual API URL
 // Generate or reuse a sessionId for the session
 function getSessionId() {
   let sessionId = sessionStorage.getItem('sessionId');
@@ -14,23 +12,17 @@ function getSessionId() {
   return sessionId;
 }
 
+
 const usePageTracker = () => {
   const location = useLocation();
   const startTimeRef = useRef(Date.now());
   const prevPathRef = useRef(location.pathname);
-  const sessionId = getSessionId();
 
   useEffect(() => {
     // Don't track admin pages
     if (location.pathname.startsWith('/admin')) return;
     const logActivity = (path, timeSpent) => {
-      console.log('Sending activity:', { page: path, timeSpent, sessionId });
-      navigator.sendBeacon(
-        api_url,
-        new Blob([
-          JSON.stringify({ page: path, timeSpent, sessionId })
-        ], { type: 'application/json' })
-      );
+      navigator.sendBeacon('https://visitor-traker.onrender.com/api/activity/log', JSON.stringify({ page: path, timeSpent }));
     };
 
     const now = Date.now();
@@ -39,25 +31,19 @@ const usePageTracker = () => {
 
     startTimeRef.current = now;
     prevPathRef.current = location.pathname;
-  }, [location, sessionId]);
+  }, [location]);
 
   useEffect(() => {
     const handleUnload = () => {
       if (location.pathname.startsWith('/admin')) return;
       const now = Date.now();
       const timeSpent = Math.round((now - startTimeRef.current) / 1000);
-      console.log('Sending activity (unload):', { page: prevPathRef.current, timeSpent, sessionId });
-      navigator.sendBeacon(
-        api_url,
-        new Blob([
-          JSON.stringify({ page: prevPathRef.current, timeSpent, sessionId })
-        ], { type: 'application/json' })
-      );
+      navigator.sendBeacon('https://visitor-traker.onrender.com/api/activity/log', JSON.stringify({ page: prevPathRef.current, timeSpent }));
     };
 
     window.addEventListener('beforeunload', handleUnload);
     return () => window.removeEventListener('beforeunload', handleUnload);
-  }, [location, sessionId]);
+  }, [location]);
 };
 
 export default usePageTracker;
