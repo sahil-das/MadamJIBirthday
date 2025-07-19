5a14849470039853ca73ac62c2a41ab6ade2c115
sahil-das <66070916+sahil-das@users.noreply.github.com>
2025-07-19 09:26:13 +0530

    Update DiaryPage.jsx

--- src/components/DiaryPage.jsx
+++ src/components/DiaryPage.jsx
@@ -73,10 +73,7 @@ export default function SurpriseSlideshow() {
                 </div>
               ) : (
                 <div className="photo-container" style={{ backgroundImage: `url(${item.src})` }}>
-                  <div className="photo-overlay-text">
-                    Madam ji, Sorry
-                    <br /> <br /> for your privacy, I used random images from the internet.
-                  </div>
+
                 </div>
               )}
               <div className="quote">{item.quote}</div>
